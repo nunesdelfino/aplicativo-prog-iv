64c6ad90f879edfbed1f40978805fb865172a96e
Maria Eduarda <dudafalcaodeoliveira@gmail.com>
2022-03-01 17:50:03 -0300

    Realização de entregas pendentes

---
+++ lib/paginaEntrega.dart
@@ -0,0 +1,179 @@
+import 'package:fabricadechocolate/entregalist.dart';
+import 'package:fabricadechocolate/models/pedido.dart';
+import 'package:fabricadechocolate/services/api_service.dart';
+import 'package:flutter/material.dart';
+
+import 'drawerMain.dart';
+
+class PaginaEntrega extends StatefulWidget {
+  PaginaEntrega({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _PaginaEntregaState createState() => _PaginaEntregaState();
+}
+
+class _PaginaEntregaState extends State<PaginaEntrega> {
+  final ApiService api = ApiService();
+  List<Pedido> entregaList;
+  List<Pedido> entregueList;
+
+  @override
+  Widget build(BuildContext context) {
+    if (entregaList == null) {
+      entregaList = [];
+    }
+    if (entregueList == null) {
+      entregueList = [];
+    }
+
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+            drawer: DrawerMain(),
+            appBar: AppBar(
+              title: Text(widget.title),
+              actions: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(right: 15),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        this.entregaList = entregaList;
+                        this.entregueList = entregueList;
+                      });
+                    },
+                    child: Icon(Icons.refresh),
+                  ),
+                )
+              ],
+              bottom: TabBar(
+                indicatorColor: Colors.yellow.shade200,
+                tabs: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(Icons.library_books),
+                      Text("   Entregas Pendentes")
+                    ],
+                  ), //Entregas
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(Icons.history),
+                      Text("   Histórico")
+                    ],
+                  ), //O que já foi entregue
+                ],
+              ),
+            ),
+            body: TabBarView(
+              children: <Widget>[
+                FutureBuilder(
+                    future: loadListEntrega(),
+                    builder: (context, snapshot) {
+                      print("feature builder");
+                      print(snapshot.connectionState);
+
+                      return entregaList.length > 0
+                          ? SingleChildScrollView(
+                              child: Container(
+                                  padding: EdgeInsets.only(top: 20),
+                                  child: EntregaList(
+                                      entregas: entregaList,
+                                      loadListSetState: _getData)))
+                          : Center(
+                              child: Text(
+                                "Sem dados",
+                                style: Theme.of(context).textTheme.bodyText1,
+                              ),
+                            );
+                    }
+
+                    //  return entregaList.length > 0
+                    //      ? new EntregaList(
+                    //          //criar classe com o nome
+                    //          entregas: entregaList)//,funcao: _getData())
+                    //      : new Center(
+                    //          child: new Text("Nenhum registro encontrado!",
+                    //              style:
+                    //                  Theme.of(context).textTheme.headline6)
+
+                    ),
+                FutureBuilder(
+                    future: loadListEntregues(),
+                    builder: (context, snapshot) {
+                      print("feature builder");
+                      print(snapshot.connectionState);
+
+                      return entregueList.length > 0
+                          ? SingleChildScrollView(
+                              child: Container(
+                                  padding: EdgeInsets.only(top: 20),
+                                  child: EntregaList(
+                                    entregas: entregueList,
+                                    loadListSetState: _getData,
+                                  )))
+                          : Center(
+                              child: Text(
+                                "Sem dados",
+                                style: Theme.of(context).textTheme.bodyText1,
+                              ),
+                            );
+                    }), /*FutureBuilder(
+                    future: loadListEntregues(),
+                    builder: (context, snapshot) {
+                      return entregueList.length > 0
+                          ? new EntregueList(
+                              entregas:
+                                  entregueList) //entregas eh a variavel da outra classe
+                          : new Center(
+                              child: new Text("Nenhum registro encontrado!",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headline6)); //Center
+                    })*/
+              ],
+            )));
+  }
+
+  Future<List<Pedido>> loadListEntrega() {
+    //async
+    print("load list");
+    Future<List<Pedido>> futureEntregas = api.entregar();
+    futureEntregas.then((entregaList) {
+      this.entregaList = entregaList;
+    });
+    return futureEntregas;
+  }
+
+//Adicionado 15/02/2022
+  Future<List<Pedido>> loadListEntregues() {
+    //async
+    print("load list");
+    Future<List<Pedido>> futureEntregues = api.entregue();
+    futureEntregues.then((entregueList) {
+      this.entregueList = entregueList;
+    });
+    return futureEntregues;
+  }
+
+//Dois SetState
+  _getData() {
+    setState(() {
+      loadListEntrega();
+      //Adicionado 15/02/2022
+      loadListEntregues();
+    });
+  }
+
+  @override
+  Future<void> initState() {
+    //async
+    super.initState();
+    loadListEntrega();
+    //Adicionado 15/02/2022
+    loadListEntregues();
+  }
+}
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,18 +4,61 @@ import 'package:fabricadechocolate/models/pedido.dart';
 import 'package:http/http.dart';
 
 class ApiService {
+  final String apiURL =
+      "http://192.168.1.104:8081/fabricadechocolate-api/api/v1/pedido/aceitos-pendentes";
+  final String apiURLModi =
+      "http://192.168.1.104:8081/fabricadechocolate-api/api/v1/pedido";
 
-  final String apiURL = "http://192.168.3.101:8081/fabricadechocolate-api/api/v1/pedido/aceitos-pendentes";
-  final String apiURLModi = "http://192.168.3.101:8081/fabricadechocolate-api/api/v1/pedido";
+  Future<List<Pedido>> entregar() async {
+    Response res = await get('$apiURLModi/entregar');
+
+    if (res.statusCode == 200) {
+      List<dynamic> body = jsonDecode(res.body);
+      List<Pedido> entregas =
+          body.map((dynamic item) => Pedido.fromJson(item)).toList();
+      return entregas;
+    } else if (res.statusCode == 404) {
+      return [];
+    } else {
+      throw "Falha ao carregar a lista de pedidos a serem entregues";
+    }
+  }
+
+  Future<List<Pedido>> entregue() async {
+    Response res = await get('$apiURLModi/entregue');
+
+    if (res.statusCode == 200) {
+      List<dynamic> body = jsonDecode(res.body);
+      List<Pedido> entregas =
+          body.map((dynamic item) => Pedido.fromJson(item)).toList();
+      return entregas;
+    }
+    if (res.statusCode == 404) {
+      return [];
+    } else {
+      throw "Falha ao carregar Lista";
+    }
+  }
+
+  Future<Pedido> updateEntregas(int id) async {
+    Response response = await put('$apiURLModi/$id/entregar-pedido');
+    if (response.statusCode == 200) {
+      return Pedido.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Falha ao atualizar a entrega');
+    }
+  }
 
   Future<List<Pedido>> getPedidos() async {
     Response res = await get(apiURL);
 
-    if(res.statusCode == 200) {
+    if (res.statusCode == 200) {
       List<dynamic> body = jsonDecode(res.body);
-      List<Pedido> p = body.map((dynamic item) => Pedido.fromJson(item)).toList();
+      List<Pedido> p =
+          body.map((dynamic item) => Pedido.fromJson(item)).toList();
       return p;
-    } if(res.statusCode == 404){
+    }
+    if (res.statusCode == 404) {
       return [];
     } else {
       throw "Falha ao carregar Lista";
